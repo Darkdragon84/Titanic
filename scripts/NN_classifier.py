6a26fc6f7daf36cfc71ab9efc19b3a6485b4e8d5
Valentin Stauber <stauber.valentin@gmail.com>
2017-09-19 18:49:54 +0200

    today's progress

--- scripts/NN_classifier.py
+++ scripts/NN_classifier.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-from collections import namedtuple
-import math
+import os
 from keras import optimizers, initializers, regularizers
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
@@ -16,16 +15,41 @@ from sklearn.pipeline import Pipeline
 seed = 42  # for reproducability
 np.random.seed(seed)
 
-train = pd.read_csv( '../input/train.csv')
-test = pd.read_csv('../input/test.csv')
+train0 = pd.read_csv('../input/train.csv')
+test0 = pd.read_csv('../input/test.csv')
 
-n_train = train.shape[0]
-n_test = test.shape[0]
+n_train = train0.shape[0]
+n_test = test0.shape[0]
 
-print('bare data')
-print(train.head(5))
+## FEATURE ENGINEERING ################################################################################################
+# a map of more aggregated titles
+Title_Dictionary = {
+    "Capt": "Officer",
+    "Col": "Officer",
+    "Major": "Officer",
+    "Jonkheer": "Royalty",
+    "Don": "Royalty",
+    "Sir": "Royalty",
+    "Dr": "Officer",
+    "Rev": "Officer",
+    "the Countess": "Royalty",
+    "Dona": "Royalty",
+    "Mme": "Mrs",
+    "Mlle": "Miss",
+    "Ms": "Mrs",
+    "Mr": "Mr",
+    "Mrs": "Mrs",
+    "Miss": "Miss",
+    "Master": "Master",
+    "Lady": "Royalty"
+}
 
-## FEATURE ENGINEERING
+titles = set()
+for _, title in Title_Dictionary.items():
+    titles.add(title)
+
+train = train0.copy()
+test = test0.copy()
 
 full_data = [train, test]
 
@@ -33,47 +57,88 @@ full_data = [train, test]
 train['Has_Cabin'] = train["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
 test['Has_Cabin'] = test["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
 
-# some of the feature engineering steps taken from Sina
 # Create new feature FamilySize as a combination of SibSp and Parch
 for dataset in full_data:
     dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+
 # Create new feature IsAlone from FamilySize
 for dataset in full_data:
     dataset['IsAlone'] = 0
     dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
-# Remove all NULLS in the Embarked column (use most common value 'S')
+
+# Remove all NULLS in the Embarked column (fill with most common category, which is 'S')
 for dataset in full_data:
     dataset['Embarked'] = dataset['Embarked'].fillna('S')
-# Remove all NULLS in the Fare column and fill with median
+
+# Remove all NULLS in the Fare column and create a new feature CategoricalFare
 for dataset in full_data:
     dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
-# Remove all NULLS in the Age column and fill random age in range[avg - std, avg + std] (questionable...)
+
+# create new feature title, extracted from full name, and map to one of 6 title categories
 for dataset in full_data:
-    age_avg = dataset['Age'].mean()
-    age_std = dataset['Age'].std()
-    age_null_count = dataset['Age'].isnull().sum()  # counts the number of NaN is the Age column
-    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
-    # fill missing ages with random ages around mean age
-    dataset.loc[dataset['Age'].isnull(), 'Age'] = age_null_random_list
-    dataset['Age'] = dataset['Age'].astype(int)
-# Map Sex to categorical [1,0]
+    dataset['Title'] = dataset['Name'].map(lambda name: name.split(',')[1].split('.')[0].strip())
+    # we map each title
+    dataset['Title'] = dataset.Title.map(Title_Dictionary)
+
+# map categorical features to numerical (consider creating dummies for non ordinal!)
 for dataset in full_data:
     # Mapping Sex
-    dataset['Sex'] = dataset['Sex'].astype("category").cat.codes
-    # Mapping Embarked
-    # dataset['Embarked'] = dataset['Embarked'].astype("category").cat.codes
+    # dataset['Sex'] = dataset['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+    dataset['Sex'] = dataset['Sex'].astype('category').cat.codes
+    # dataset['Embarked'] = dataset['Embarked'].astype('category').cat.codes
 
-# create dummies for Embarked (unordered categorical feature)
-# why does this not work if done in the above for loop (it has no effect)
+# For the age we do sth a bit more elaborate. Instead of using the median of the entire dataset
+# we calculate the median age for a finer grouping, namely by (Sex, Pclass, Title)
+# We then fill all the null age values with the median corresponding to (Sex, Pclass, Title) of that entry
+
+# calculate median of ENTIRE dataset (including test)
+combined = train.append(test)
+combined.reset_index(inplace=True)  # probably not necessary, but do it anyway
+combined.drop('index', inplace=True, axis=1)  # probably not necessary, but do it anyway
+age_medians = combined[['Sex', 'Pclass', 'Title', 'Age']].groupby(['Sex', 'Pclass', 'Title']).median()
+
+def fill_age(row):
+    return age_medians.loc[(row['Sex'], row['Pclass'], row['Title']), 'Age']
+
+for dataset in full_data:
+    age_msk = dataset['Age'].isnull()  # select entries with missing age
+    dataset.loc[age_msk, 'Age'] = dataset.loc[age_msk].apply(fill_age, axis=1)
+
 train = pd.get_dummies(train, columns=['Embarked'])
 test = pd.get_dummies(test, columns=['Embarked'])
 
-drop_elements = ['Name', 'Ticket', 'Cabin', 'SibSp', 'Parch']
-train = train.drop(drop_elements, axis=1)
-test = test.drop(drop_elements, axis=1)
+train = pd.get_dummies(train, columns=['Title'])
+test = pd.get_dummies(test, columns=['Title'])
+########################################################################################################################
+
+## These features and parameters gave me a LB score of 0.77990, which is not great unfortunately...
+wanted_features = ['Pclass', 'Sex', 'Age',
+                   'Fare', 'Has_Cabin',
+                   'FamilySize',
+                   'Embarked_Q', 'Embarked_S',
+                   'Title_Miss', 'Title_Mr', 'Title_Mrs', 'Title_Officer', 'Title_Royalty']
 
 
-wanted_features = ['Pclass', 'Sex', 'Age', 'Fare', 'Has_Cabin', 'IsAlone', 'Embarked_C', 'Embarked_Q', 'Embarked_S']
+### NN parameters
+
+epochs = 800
+batch_size = 50
+optim = 'rmsprop'
+dim1 = 50
+dim2 = 50
+dim3 = 10
+
+# rg = 1e-3
+# init = initializers.RandomUniform(minval=-rg, maxval=rg)
+init_W = initializers.glorot_normal(seed)
+init_b = initializers.Zeros()
+# reg = None
+# reg_val = 0.01
+reg_val = 0.2
+# reg = regularizers.l2(reg_val)
+reg = None
+input_dim = None
+
 
 X_train = train[wanted_features].values
 X_test = test[wanted_features].values
@@ -89,33 +154,34 @@ scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-### NN parameters
-rg = 1e-3
-
-epochs = 400
-batch_size = 10
-optim = 'adam'
-
-init = initializers.RandomUniform(minval=-rg, maxval=rg)
-reg = regularizers.l2(0.002)
-
 model = Sequential()
-model.add(Dense(30, input_dim=n_features,
+
+model.add(Dropout(reg_val, input_shape=(n_features,)))
+model.add(Dense(dim1,
                 activation='relu',
-                kernel_initializer=init,
-                bias_initializer=init,
+                kernel_initializer=init_W,
+                bias_initializer=init_b,
                 kernel_regularizer=reg,
                 bias_regularizer=reg))
 
-model.add(Dense(20, activation='relu',
-                kernel_initializer=init,
-                bias_initializer=init,
+model.add(Dropout(reg_val))
+model.add(Dense(dim2, activation='relu',
+                kernel_initializer=init_W,
+                bias_initializer=init_b,
                 kernel_regularizer=reg,
                 bias_regularizer=reg))
 
+model.add(Dropout(reg_val))
+model.add(Dense(dim3, activation='relu',
+                kernel_initializer=init_W,
+                bias_initializer=init_b,
+                kernel_regularizer=reg,
+                bias_regularizer=reg))
+
+model.add(Dropout(reg_val))
 model.add(Dense(1, activation='sigmoid',
-                kernel_initializer=init,
-                bias_initializer=init,
+                kernel_initializer=init_W,
+                bias_initializer=init_b,
                 kernel_regularizer=reg,
                 bias_regularizer=reg))
 
@@ -130,7 +196,9 @@ surv_msk = Y_final > 0.5
 Y_final = np.zeros(Y_final.shape)
 Y_final[surv_msk] = 1
 
+file_name = 'predictions_MLP_Keras_Dims' + str(dim1) + '_' + str(dim2) + '_' + str(dim3) + '_RegDropout' + str(reg_val) + '_Optim' + \
+            optim.capitalize() + '_Epochs' + str(epochs) + '_BatchSize' + str(batch_size) + '.csv'
 test['Survived'] = Y_final.astype('int64')
 df_out = test[['PassengerId', 'Survived']]
-df_out.head(10)
-df_out.to_csv('../output/predictions_MLP_Keras.csv', index=False)
+
+df_out.to_csv(os.path.join('../output', file_name), index=False)
--- scripts/NN_classifier_CV.py
+++ scripts/NN_classifier_CV.py
@@ -16,16 +16,41 @@ from sklearn.pipeline import Pipeline
 seed = 42  # for reproducability
 np.random.seed(seed)
 
-train = pd.read_csv( '../input/train.csv')
-test = pd.read_csv('../input/test.csv')
+train0 = pd.read_csv('../input/train.csv')
+test0 = pd.read_csv('../input/test.csv')
 
-n_train = train.shape[0]
-n_test = test.shape[0]
+n_train = train0.shape[0]
+n_test = test0.shape[0]
 
-print('bare data')
-print(train.head(5))
+## FEATURE ENGINEERING ################################################################################################
+# a map of more aggregated titles
+Title_Dictionary = {
+    "Capt": "Officer",
+    "Col": "Officer",
+    "Major": "Officer",
+    "Jonkheer": "Royalty",
+    "Don": "Royalty",
+    "Sir": "Royalty",
+    "Dr": "Officer",
+    "Rev": "Officer",
+    "the Countess": "Royalty",
+    "Dona": "Royalty",
+    "Mme": "Mrs",
+    "Mlle": "Miss",
+    "Ms": "Mrs",
+    "Mr": "Mr",
+    "Mrs": "Mrs",
+    "Miss": "Miss",
+    "Master": "Master",
+    "Lady": "Royalty"
+}
 
-## FEATURE ENGINEERING
+titles = set()
+for _, title in Title_Dictionary.items():
+    titles.add(title)
+
+train = train0.copy()
+test = test0.copy()
 
 full_data = [train, test]
 
@@ -33,62 +58,113 @@ full_data = [train, test]
 train['Has_Cabin'] = train["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
 test['Has_Cabin'] = test["Cabin"].apply(lambda x: 0 if type(x) == float else 1)
 
-# some of the feature engineering steps taken from Sina
 # Create new feature FamilySize as a combination of SibSp and Parch
 for dataset in full_data:
     dataset['FamilySize'] = dataset['SibSp'] + dataset['Parch'] + 1
+
 # Create new feature IsAlone from FamilySize
 for dataset in full_data:
     dataset['IsAlone'] = 0
     dataset.loc[dataset['FamilySize'] == 1, 'IsAlone'] = 1
-# Remove all NULLS in the Embarked column (use most common value 'S')
+
+# Remove all NULLS in the Embarked column (fill with most common category, which is 'S')
 for dataset in full_data:
     dataset['Embarked'] = dataset['Embarked'].fillna('S')
-# Remove all NULLS in the Fare column and fill with median
+
+# Remove all NULLS in the Fare column and create a new feature CategoricalFare
 for dataset in full_data:
     dataset['Fare'] = dataset['Fare'].fillna(train['Fare'].median())
-# Remove all NULLS in the Age column and fill random age in range[avg - std, avg + std] (questionable...)
+
+# create new feature title, extracted from full name, and map to one of 6 title categories
 for dataset in full_data:
-    age_avg = dataset['Age'].mean()
-    age_std = dataset['Age'].std()
-    age_null_count = dataset['Age'].isnull().sum()  # counts the number of NaN is the Age column
-    age_null_random_list = np.random.randint(age_avg - age_std, age_avg + age_std, size=age_null_count)
-    # fill missing ages with random ages around mean age
-    dataset.loc[dataset['Age'].isnull(), 'Age'] = age_null_random_list
-    dataset['Age'] = dataset['Age'].astype(int)
-# Map Sex to categorical [1,0]
+    dataset['Title'] = dataset['Name'].map(lambda name: name.split(',')[1].split('.')[0].strip())
+    # we map each title
+    dataset['Title'] = dataset.Title.map(Title_Dictionary)
+
+# map categorical features to numerical (consider creating dummies for non ordinal!)
 for dataset in full_data:
     # Mapping Sex
-    dataset['Sex'] = dataset['Sex'].astype("category").cat.codes
-    # Mapping Embarked
-    dataset['Embarked'] = dataset['Embarked'].astype("category").cat.codes
+    # dataset['Sex'] = dataset['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+    dataset['Sex'] = dataset['Sex'].astype('category').cat.codes
+    # dataset['Embarked'] = dataset['Embarked'].astype('category').cat.codes
 
-# create dummies for Embarked (unordered categorical feature)
-# why does this not work if done in the above for loop (it has no effect)
-# train = pd.get_dummies(train, columns=['Embarked'])
-# test = pd.get_dummies(test, columns=['Embarked'])
+# For the age we do sth a bit more elaborate. Instead of using the median of the entire dataset
+# we calculate the median age for a finer grouping, namely by (Sex, Pclass, Title)
+# We then fill all the null age values with the median corresponding to (Sex, Pclass, Title) of that entry
 
-drop_elements = ['Name', 'Ticket', 'Cabin', 'SibSp', 'Parch']
-train = train.drop(drop_elements, axis = 1)
-test = test.drop(drop_elements, axis = 1)
+# calculate median of ENTIRE dataset (including test)
+combined = train.append(test)
+combined.reset_index(inplace=True)  # probably not necessary, but do it anyway
+combined.drop('index', inplace=True, axis=1)  # probably not necessary, but do it anyway
+age_medians = combined[['Sex', 'Pclass', 'Title', 'Age']].groupby(['Sex', 'Pclass', 'Title']).median()
 
-print('cleaned dataset')
-print(train.head(10))
+
+def fill_age(row):
+    return age_medians.loc[(row['Sex'], row['Pclass'], row['Title']), 'Age']
+
+
+for dataset in full_data:
+    age_msk = dataset['Age'].isnull()  # select entries with missing age
+    dataset.loc[age_msk, 'Age'] = dataset.loc[age_msk].apply(fill_age, axis=1)
+
+# create additional ordinal categorical age feature
+n_age_bins = 5
+train['CategoricalAge'], age_bins = pd.cut(train['Age'], n_age_bins, retbins=True)
+
+train = pd.get_dummies(train, columns=['Embarked'], drop_first=True)
+test = pd.get_dummies(test, columns=['Embarked'], drop_first=True)
+
+train = pd.get_dummies(train, columns=['Title'], drop_first=True)
+test = pd.get_dummies(test, columns=['Title'], drop_first=True)
+########################################################################################################################
+
+# drop_elements = ['Name', 'Ticket', 'Cabin', 'SibSp', 'Parch']
+# train = train.drop(drop_elements, axis = 1)
+# test = test.drop(drop_elements, axis = 1)
+
+
 
 #wanted_features = ['Pclass', 'Sex', 'Age', 'Fare', 'Has_Cabin', 'FamilySize', 'IsAlone',\
                    #'Embarked_C', 'Embarked_Q', 'Embarked_S']
-wanted_features = ['Pclass', 'Sex', 'Age', 'Fare', 'Has_Cabin', 'IsAlone', 'Embarked']
+# wanted_features = ['Pclass', 'Sex', 'Age', 'Fare', 'Has_Cabin', 'IsAlone', 'FamilySize', 'Embarked']
+# wanted_features = ['Pclass', 'Sex', 'Age', 'Fare', 'Has_Cabin', 'IsAlone', 'FamilySize',
+#                    'Embarked_C', 'Embarked_Q', 'Embarked_S',
+#                    'Title_Master', 'Title_Miss', 'Title_Mr', 'Title_Mrs', 'Title_Officer', 'Title_Royalty']
+wanted_features = ['Pclass', 'Sex', 'Age',
+                   'Fare', 'Has_Cabin',
+                   'FamilySize',
+                   'Embarked_Q', 'Embarked_S',
+                   'Title_Miss', 'Title_Mr', 'Title_Mrs', 'Title_Officer', 'Title_Royalty']
 # wanted_features = ['Sex', 'Age', 'Fare', 'Has_Cabin', 'FamilySize']
 # wanted_features = ['Pclass', 'Sex', 'Age', 'Fare', 'Has_Cabin', 'IsAlone', 'Embarked_C', 'Embarked_Q', 'Embarked_S']
 
+reginfo = namedtuple('reginfo',['type', 'value'])
+
+## build pipeline
+n_splits = 10
+epochs = 800
+batch_size = 50
+optim = 'rmsprop'
+hidden_dims = [50, 50, 10]
+n_regs = len(hidden_dims) + 1
+reginfos = n_regs*[reginfo('dropout', 0.2)]
+# reginfos = n_regs*[reginfo('l2', 0.02)]
+# reginfos = None
+# reginfos = n_regs*[reginfo('l2', 0.01)]
+# reginfos = n_regs*[reginfo('dropout', 0.25)]
+
+sk_params = {'epochs': epochs,
+             'batch_size': batch_size,
+             'optim': optim,
+             'hidden_dims': hidden_dims,
+             'reginfos': reginfos}
+
 X_train = train[wanted_features].values
 X_test = test[wanted_features].values
-
 Y_train = train['Survived'].values
 
 n_features = X_train.shape[1]
 
-reginfo = namedtuple('reginfo',['type', 'value'])
 
 ## model generator
 def build_model(hidden_dims=[10], reginfos=[reginfo('dropout', 0)], optim='adam', verbose=True):
@@ -105,7 +181,7 @@ def build_model(hidden_dims=[10], reginfos=[reginfo('dropout', 0)], optim='adam'
     # rg = 1e-3
     # init = initializers.RandomUniform(minval=-rg, maxval=rg)
     init_W = initializers.glorot_normal(seed)
-    init_b = initializers.Zeros
+    init_b = initializers.Zeros()
     model = Sequential()
 
     # rg = math.sqrt(6/(n_features + hidden_dims[0]))
@@ -161,30 +237,13 @@ def build_model(hidden_dims=[10], reginfos=[reginfo('dropout', 0)], optim='adam'
     return model
 
 
-## build pipeline
-epochs = 400
-batch_size = 10
-optim = 'adam'
-hidden_dims = [7, 3]
-# reginfos = 3*[reginfo('dropout', 0.25)]
-# reginfos = 3*[reginfo('l2', 0.02)]
-# reginfos = None
-reginfos = 3*[reginfo('l2', 0.001)]
-# reginfos = 3*[reginfo('dropout', 0.25)]
-
-sk_params = {'epochs': epochs,
-             'batch_size': batch_size,
-             'optim': optim,
-             'hidden_dims': hidden_dims,
-             'reginfos': reginfos}
-
-estimators = []
-estimators.append(('standardize', StandardScaler()))
-estimators.append(('mlp',
-                   KerasClassifier(build_fn=build_model, verbose=1, **sk_params)))
+estimators = [
+        ('standardize', StandardScaler()),
+        ('mlp', KerasClassifier(build_fn=build_model, verbose=1, **sk_params))
+    ]
 
 pipeline = Pipeline(estimators)
-kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=seed)
 results = cross_val_score(pipeline, X_train, Y_train, cv=kfold)
 
 print('\n\n')
@@ -192,4 +251,4 @@ print('features:')
 print(wanted_features)
 print('parameters:')
 print(sk_params)
-print("Standardized: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+print('Standardized: %.2f%% (%.2f%%)' % (results.mean()*100, results.std()*100))
